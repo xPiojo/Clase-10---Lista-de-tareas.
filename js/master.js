02be0dc6660946990d38bda5ea490bb79e267746
Roberto Gomez <nidhug212@gmail.com>
2023-09-05 16:33:20 -0300

    Agregada función para filtrar tareas

--- js/master.js
+++ js/master.js
@@ -11,25 +11,46 @@ taskInput.addEventListener("keypress", (event) => {
   }
 });
 
+// Agregar tarea
 function addTask() {
   const inputValue = taskInput.value.trim();
   const newTarea = document.createElement("li");
   if (inputValue !== "") {
     taskList.append(newTarea);
     newTarea.textContent = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
-    //Agrego la tarea con su primera letra en mayuscula
+    //Agrega la tarea con su primera letra en mayuscula
     taskInput.value = "";
   } else {
     alert("No ingresaste ninguna tarea");
   }
 }
 
+// Borrar tarea
 function deleteTask(element) {
   if (confirm("Desea eliminar la tarea")) {
     element.target.remove();
   }
 }
 
+// Completar tarea
 function completeTask(element) {
   element.target.classList.toggle("completeTask");
+}
+
+// Agrego funcion para filtrar por busqueda
+const inputFilter = document.getElementById("search-Filter");
+inputFilter.addEventListener("keyup", filterTasks);
+
+function filterTasks() {
+  const taskItems = document.querySelectorAll("#taskList li");
+  const filterText = inputFilter.value.toLowerCase();
+
+  taskItems.forEach((taskItem) => {
+    const taskText = taskItem.textContent.toLowerCase();
+    if (taskText.includes(filterText)) {
+      taskItem.classList.remove("hidden");
+    } else {
+      taskItem.classList.add("hidden");
+    }
+  });
 }
